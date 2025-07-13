8d2029d48cd97062b1f54141e7572f5bb2588fc5
Vibhanshu Vaibhav <vibhanshuvaibhav012@gmail.com>
2025-07-13 14:35:41 +0530

    register implemented

--- frontend_vite/src/components/Register.jsx
+++ frontend_vite/src/components/Register.jsx
@@ -1,16 +1,39 @@
 import React, { useState } from 'react'
-
+import axios from 'axios'
 const Register = () => {
     const [username,setUserName] = useState("")
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
+    const [success,setsetSucess] = useState("")
+    const [error,setError] = useState("")
+    const [isLoading,setIsLoading] = useState(false)
+//  fields = ["username",'email','password']
 
-    const handleRegister =  (e) =>{
+    const handleRegister = async (e) =>{
         e.preventDefault()
+        setIsLoading(true)
         const formData = {
             username,email,password
         }
         console.log(formData)
+        const Url = import.meta.env.VITE_URL 
+        console.log(Url)
+        try{
+        const responce = await axios.post(Url,formData)
+        console.log(responce.data)
+        console.log("success")
+        setsetSucess(true)
+        setError({})
+        }
+        catch(error){
+            console.log("error ",error)
+            // setsetSucess(`Error came: ${error.message}`)
+            setError(error.response.data)
+            
+        }
+        finally{
+            setIsLoading(false)
+        }
     }
   return (
     <>
@@ -20,9 +43,14 @@ const Register = () => {
                 <h1 className='text-light text-center mb-4'>Create a Account</h1>
                 <form onSubmit={handleRegister}>
                     <input type='text' value = {username} className='form-control mb-3' onChange={(e)=>setUserName(e.target.value)} placeholder='Enter Username'/>
+                    {error.username && <div className='text-light'>{error.username}</div>}
                     <input type='email' value = {email} className='form-control mb-3' onChange={(e)=>setEmail(e.target.value)} placeholder='Enter Email'/>
                     <input type='password' value = {password} className='form-control mb-5' onChange={(e)=>setPassword(e.target.value)} placeholder='Enter Password'/>
-                    <button type='submit' className='btn btn-info d-flex mx-auto'>Register</button>
+                    {error.password && <div className='text-danger'>{error.password}</div>}
+                    {success && <div className='text-success'>Succefully </div> }
+                    {isLoading ? (<button type='submit' className='btn btn-info d-flex mx-auto' disabled>Loading</button>)
+                    :(<button type='submit' className='btn btn-info d-flex mx-auto'>Register</button>)}
+                    
                 </form>
             </div>
         </div>
